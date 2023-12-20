4dbec591be3f96f456d999038f1f869f6a49d87a
tiago3monteiro <up202108391@example.fe.up.pt>
2023-12-20 14:31:14 +0000

    first commit

---
+++ Application.cpp
@@ -0,0 +1,5 @@
+//
+// Created by tiagomonteiro on 12/20/23.
+//
+
+#include "Application.h"
---
+++ Application.h
@@ -0,0 +1,15 @@
+//
+// Created by tiagomonteiro on 12/20/23.
+//
+
+#ifndef AED2324_PRJ2_G04_APPLICATION_H
+#define AED2324_PRJ2_G04_APPLICATION_H
+
+
+class Application {
+
+
+};
+
+
+#endif //AED2324_PRJ2_G04_APPLICATION_H
