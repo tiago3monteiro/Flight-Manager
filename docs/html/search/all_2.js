186ec36cb2eb17d7f484e9a702ed5480d6a5aae6
tiago3monteiro <up202108391@example.fe.up.pt>
2024-01-02 18:53:09 +0000

    doxy

---
+++ docs/html/search/all_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['dfs_5fessential_0',['dfs_essential',['../_application_8cpp.html#a861caf524aebfb56141f142d53c1da24',1,'Application.cpp']]],
+  ['distancetopoint_1',['distanceToPoint',['../class_airport.html#ab10fe59f2d97c1e3e7422fc82da81c06',1,'Airport']]]
+];
---
+++ docs/html/search/functions_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['essencialairports_0',['essencialAirports',['../class_application.html#af1f9030b402bd07b78534bf371ca5c12',1,'Application']]]
+];
